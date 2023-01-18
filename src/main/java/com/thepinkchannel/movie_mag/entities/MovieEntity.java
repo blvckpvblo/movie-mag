d6a6535c14a4d09e5dcdd78e5e4c28b48f4781f6
Momar Cisse <momar@thepinkchannel.com>
2023-01-18 10:47:51 +0000

    Added MovieEntity

---
+++ src/main/java/com/thepinkchannel/movie_mag/entities/MovieEntity.java
@@ -0,0 +1,78 @@
+package com.thepinkchannel.movie_mag.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movie")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "cover")
+    private String cover;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "year", nullable = false)
+    private int year;
+
+    public MovieEntity() {
+    }
+
+    public MovieEntity(String title, String cover, String description, int year) {
+        this.title = title;
+        this.cover = cover;
+        this.description = description;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
