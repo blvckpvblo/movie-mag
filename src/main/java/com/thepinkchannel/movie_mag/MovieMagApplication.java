3f69d9221a02dc120e765702d31270fac76a0ae0
Momar Cisse <momar@thepinkchannel.com>
2023-01-18 09:39:38 +0000

    Init Project

---
+++ src/main/java/com/thepinkchannel/movie_mag/MovieMagApplication.java
@@ -0,0 +1,13 @@
+package com.thepinkchannel.movie_mag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMagApplication.class, args);
+	}
+
+}
